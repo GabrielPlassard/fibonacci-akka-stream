b1c53430c330d38279e086174e25b01b385bbf22
Gabriel Plassard <gabriel.plassard@gmail.com>
2016-11-27 17:52:48 +0100

    Use runnable graph

--- src/main/scala/fr/gplassard/fibonaccistream/App.scala
+++ src/main/scala/fr/gplassard/fibonaccistream/App.scala
@@ -2,8 +2,8 @@ package fr.gplassard.fibonaccistream
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, MergePreferred, Source}
+import akka.stream.{ActorMaterializer, SourceShape}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -16,8 +16,23 @@ object App {
   object Fibonacci {
     val one = Fibonacci(1, 1, 0)
 
-    val source: Source[Fibonacci, NotUsed] = Source.single(Fibonacci.one)
+    val source: Source[Fibonacci, NotUsed] = Source.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
 
+      val seed = Source.single(Fibonacci.one)
+
+      val next = Flow[Fibonacci]
+        .map(_.next())
+
+      val bcast = builder.add(Broadcast[Fibonacci](2))
+      val merge = builder.add(MergePreferred[Fibonacci](1))
+
+
+      seed ~> merge           ~>         bcast
+              merge.preferred <~ next <~ bcast
+
+      SourceShape(bcast.out(1))
+    })
   }
 
   def main(args: Array[String]): Unit = {
@@ -25,8 +40,8 @@ object App {
     implicit val materializer = ActorMaterializer()
 
     Fibonacci.source
-      .takeWhile(_.value < 50)
+      .takeWhile(_.value < 500)
       .runForeach(println)
-      .onComplete( _ => system.terminate())
+      .onComplete(_ => system.terminate())
   }
 }
